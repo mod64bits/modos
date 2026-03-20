f287668bb56ded0c68a7258c13e74415c34c4435
mod64bits <mod64bits@gmail.com>
2026-03-20 00:48:36 +0000

    abrir chamados

--- apps/dashboard/urls.py
+++ apps/dashboard/urls.py
@@ -5,7 +5,7 @@ from .views import DashboardUserView, DashboardAdminView, AssignTicketView
 app_name = "dashboard"
 
 urlpatterns = [
-    path("", DashboardUserView.as_view(), name="dashbord_user"),
+    path("", DashboardUserView.as_view(), name="dashboard_user"),
     path("dashboard/admin/", DashboardAdminView.as_view(), name="dashboard_admin"),
     # Ação de puxar a O.S. para o técnico logado
     path("admin/atribuir/<uuid:pk>/", AssignTicketView.as_view(), name="assign_ticket"),
